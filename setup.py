f4b136af38dc15bd1bce3cd7072a9c0f00097984
Pavel Khanin <Pavel.Khanin@gmail.com>
2021-04-04 15:35:01 +0300

    test release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='pong_game',
+    name='msu_pong_game',
     packages=['client'],
     entry_points = {
         'console_scripts': ['pong_game=client.__main__:main'],
