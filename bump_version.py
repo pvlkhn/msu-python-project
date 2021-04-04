36fc4dca5817fa96424a5ad84dd9b1c5a48b3c88
Pavel Khanin <pvlkhn@sandbox-408052664>
2021-04-04 22:44:37 +0300

    Add linter

--- bump_version.py
+++ bump_version.py
@@ -1,5 +1,5 @@
 VERSION_PATH = 'client/VERSION'
 
 version_parts = open(VERSION_PATH).read().split('.')
-version_parts[-1] = str(int(version_parts[-1]) +1)
+version_parts[-1] = str(int(version_parts[-1]) + 1)
 open(VERSION_PATH, 'w').write(".".join(version_parts))
--- client/__main__.py
+++ client/__main__.py
@@ -1,11 +1,6 @@
-from  . import view
-from  . import controller
-from  . import model
-
-
 def main():
-	print("client launched")
+    print("client launched")
 
 
 if __name__ == '__main__':
-	main()
+    main()
