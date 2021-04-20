767d45845f220aad55d72152d76eea08b1e40328
Alexander Osadchy <a.i.osadchy@gmail.com>
2021-04-21 02:00:01 +0300

    Change window layout from 1x1 grid to pack, remove command line initialization for network connection

--- client/__main__.py
+++ client/__main__.py
@@ -1,25 +1,20 @@
 from .model import GameState, NetworkConnection
-from .view import GameWindow
+from .view import GameWindow, LobbyBrowserWindow
 from .controller import Controller, GameLogicController
 
-from sys import argv
-
-import tkinter
+import tkinter as tk
 
 
-class Application(tkinter.Tk):
+class Application(tk.Tk):
     WIDTH = 800
     HEIGHT = 600
 
-    def __init__(self, host, port):
+    def __init__(self):
         super().__init__()
-
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
 
         game_state = GameState(Application.WIDTH, Application.HEIGHT)
         game_controller = GameLogicController(game_state)
-        server_connection = NetworkConnection(host, port)
+        server_connection = NetworkConnection("localhost", 228)
         controller = Controller(
             game_controller=game_controller,
             platform_index=0,  # TODO: use 0 for host, 1 for connected
@@ -30,13 +25,14 @@ class Application(tkinter.Tk):
         self.minsize(Application.WIDTH, Application.HEIGHT)
 
         window = GameWindow(game_state, controller, 40, 100, master=self)
-        window.grid(sticky="NWSE")
+        window.pack(expand=True, fill=tk.BOTH)
+
+        # self.lobby_browser = LobbyBrowserWindow(master=self)
+        # self.lobby_browser.pack(fill=tk.BOTH)
 
 
 def main():
-    # TODO: game selection via GUI
-    assert len(argv) >= 3
-    Application(host=argv[1], port=int(argv[2])).mainloop()
+    Application().mainloop()
 
 
 if __name__ == '__main__':
